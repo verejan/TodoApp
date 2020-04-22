c9058d865a2ab52af5754614bb1b77f4899e8230
verejan <verejancr@gmail.com>
2020-04-22 16:47:28 +0300

    Create Intray top-bar and button #2

--- App/todoapp/lib/main.dart
+++ App/todoapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/global.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'TodoApp Home Page'),
     );
   }
 }
@@ -40,14 +41,84 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-       
-        title: Text(widget.title),
+    return SafeArea(
+    child: MaterialApp(
+      color: Colors.blue,
+      home: DefaultTabController(
+        length: 3,
+        child: new Scaffold(
+          body: Stack(
+                children: <Widget>[
+                  TabBarView(
+              children: [
+                new Container(
+                  color: darkGreyColor,
+                ),
+                new Container(
+                  color: Colors.orange,
+                  ),
+                new Container(
+                  color: Colors.lightGreen,
+                ),
+              ],
+                
+            ),
+            Container(
+              padding: EdgeInsets.only(left:50),
+              height: 160,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50)
+                ) ,
+                color: Colors.white
+
+                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                Text("Intray", style: intrayTitleStyle,),
+                Container()
+                ],
+              ) ,
+            ),
+            Container(
+              width: 70,
+              height: 70,
+              margin: EdgeInsets.only(top: 120, left: MediaQuery.of(context).size.width*0.5-40),
+              child: FloatingActionButton(
+                child: Icon(Icons.add, size: 60,),
+                backgroundColor: redColor,
+                onPressed: (){},
+              ),
+            )
+            ]
+          ),
+          appBar: AppBar(
+              elevation: 0,
+              title: new TabBar(
+              tabs: [
+                Tab(
+                  icon: new Icon(Icons.home),
+                ),
+                Tab(
+                  icon: new Icon(Icons.rss_feed),
+                ),
+                Tab(
+                  icon: new Icon(Icons.perm_identity),
+                )
+              ],
+              labelColor: darkGreyColor,
+              unselectedLabelColor: Colors.blue,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorPadding: EdgeInsets.all(5.0),
+              indicatorColor: Colors.transparent,
+            ),
+            backgroundColor: Colors.white,
+          ),
+        ),
       ),
-      body: Center(
-      ),
+    ),
     );
   }
 }
