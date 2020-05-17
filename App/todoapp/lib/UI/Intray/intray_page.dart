e830822bf71a2b9d6dbbe198b48c803c17bb303f
verejan <verejancr@gmail.com>
2020-05-17 15:23:07 +0300

    Some changes

--- App/todoapp/lib/UI/Intray/intray_page.dart
+++ App/todoapp/lib/UI/Intray/intray_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:todoapp/models/global.dart';
+import 'package:todoapp/models/widgets/intray_todo_widget.dart';
 
 
 class IntrayPage extends StatefulWidget {
@@ -10,10 +11,21 @@ class IntrayPage extends StatefulWidget {
 
 class _IntrayPageState extends State <IntrayPage>{
   @override
-  Widget build(Buildcontext context) {
+  Widget build(BuildContext context) {
     return Container(
       color: darkGreyColor,
-      child: ListView,
+      child: ListView(
+        padding: EdgeInsets.only(top: 300),
+        children: getList(),
+      )
     )
+  }
+
+  List<Widget> getList() {
+    List<IntrayTodo> list = [];
+    for (int i = 0; i < 10; i++) {
+      list.add(IntrayTodo());
+    }
+    return list;
   }
 }
